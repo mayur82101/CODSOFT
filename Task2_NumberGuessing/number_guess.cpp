543360de85588dc8cbb33b3763d64f1bb3d2094e
Mayur Dhone <mayurdhone82@gmail.com>
2025-12-18 22:06:29 +0530

    Update number_guess.cpp

--- Task2_NumberGuessing/number_guess.cpp
+++ Task2_NumberGuessing/number_guess.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int number, guess;
+
+    // Seed random number generator
+    srand(time(0));
+
+    // Generate random number between 1 and 100
+    number = rand() % 100 + 1;
+
+    cout << "🎲 Number Guessing Game\n";
+    cout << "Guess a number between 1 and 100\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > number) {
+            cout << "Too high! Try again.\n";
+        } 
+        else if (guess < number) {
+            cout << "Too low! Try again.\n";
+        } 
+        else {
+            cout << "🎉 Congratulations! You guessed the correct number.\n";
+        }
+
+    } while (guess != number);
+
+    return 0;
+}
 
